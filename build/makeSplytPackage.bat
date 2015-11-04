0436210036bc98605fdfb3dbd549533c018661dc
Brandon Ray <brandon.ray@knetik.com>
2015-11-04 12:50:31 -0500

    SPLYT-372: Updated build scripts. Removed more temp files.

--- build/makeSplytPackage.bat
+++ build/makeSplytPackage.bat
@@ -2,9 +2,9 @@
 @SET BASEDIR=%cd%
 @ECHO Making splyt package...
 @ECHO %BASEDIR%
+@ECHO Builder.MakeSplytPackage
 "C:\Program Files\Unity\Editor\Unity" -batchmode -quit -projectPath "%BASEDIR%\..\SplytUnity" -executeMethod Builder.MakeSplytPackage
+@ECHO Builder.MakeBPPackage
 "C:\Program Files\Unity\Editor\Unity" -batchmode -quit -projectPath "%BASEDIR%\..\SplytUnity" -executeMethod Builder.MakeBPPackage
-REM "C:\Program Files\Unity\Editor\Unity" -batchmode -quit -projectPath "%BASEDIR%\..\samples\BubblePop" -executeMethod Builder.MakeSplytPackage
-REM "C:\Program Files\Unity\Editor\Unity" -batchmode -quit -projectPath "%BASEDIR%\..\samples\BubblePop" -executeMethod Builder.MakeBPPackage
 @ECHO Done.
 @PAUSE
